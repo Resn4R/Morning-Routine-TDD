0f81ff025ea8ffd072f8817d66327f3346fc1f5d
Vito <purview-joust.0x@icloud.com>
2024-03-07 19:04:36 +0000

    first commit, one test green

---
+++ Morning Routine TDD/ReminderModel.swift
@@ -0,0 +1,15 @@
+//
+//  ReminderModel.swift
+//  Morning Routine TDD
+//
+//  Created by Vito Borghi on 07/03/2024.
+//
+
+import Foundation
+
+struct Reminder {
+    func morningRoutine(_ hour: Int, _ min: Int) -> String {
+        if hour >= 6 && hour < 7 { return "Do Exercise" }
+        else { return "Buttocks" }
+    }
+}
--- Morning Routine TDDTests/Morning_Routine_TDDTests.swift
+++ Morning Routine TDDTests/Morning_Routine_TDDTests.swift
@@ -9,28 +9,15 @@ import XCTest
 @testable import Morning_Routine_TDD
 
 final class Morning_Routine_TDDTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testWhenInputIsBetween6And7ReturnExercise () {
+        let hour = 6
+        let min = 30
+        
+        let sample = Reminder()
+        
+        XCTAssertEqual(sample.morningRoutine(hour, min), "Do Exercise")
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+    
 }

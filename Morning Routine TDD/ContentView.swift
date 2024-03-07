c0d7732fec7891743c95cc41cd095c94de83d2a3
Vito <purview-joust.0x@icloud.com>
2024-03-07 18:44:04 +0000

    Initial Commit

---
+++ Morning Routine TDD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Morning Routine TDD
+//
+//  Created by Vito Borghi on 07/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Morning Routine TDD/Morning_Routine_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morning_Routine_TDDApp.swift
+//  Morning Routine TDD
+//
+//  Created by Vito Borghi on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Morning_Routine_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

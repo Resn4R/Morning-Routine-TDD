cfea7d5fcf9c4bea20b9244b5c2ea4af9b898eba
Vito <purview-joust.0x@icloud.com>
2024-03-09 14:19:20 +0000

    created routine protocol, beginning abstraction of things

---
+++ Morning Routine TDD/RoutineProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  RoutineModel.swift
+//  Morning Routine TDD
+//
+//  Created by Vito Borghi on 09/03/2024.
+//
+
+import Foundation
+
+protocol Routine {
+    var activities: [String] { get set }
+    
+    func getActivity(atHour hour: Int, atMinute minute: Int)
+}
